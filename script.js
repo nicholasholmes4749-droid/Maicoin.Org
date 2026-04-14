dee2b33af0422cb60204ce3a0cc6cca83037a669
nicholasholmes4749-droid <nicholasholmes4749@gmail.com>
2026-04-14 17:11:47 +0100

    Create script.js
    Update full structure script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Connect Wallet
+async function connectWallet() {
+  if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      const walletAddress = accounts[0];
+      document.getElementById("walletAddress").innerText =
+        "Connected: " + walletAddress;
+
+      console.log("Wallet connected:", walletAddress);
+
+    } catch (error) {
+      console.error("Connection failed", error);
+    }
+  } else {
+    alert("Please install MetaMask!");
+  }
+}
